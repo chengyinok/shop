95352e360670f3b209a8fdd55ea3d3f0540ff5f3
hanlifeng <hanlifeng@qq.com>
2020-10-20 22:26:42 +0800

    init project

--- src/main/java/com/shop/exception/GlobalExceptionHandler.java
+++ src/main/java/com/shop/exception/GlobalExceptionHandler.java
@@ -1,17 +1,13 @@
 package com.shop.exception;
 
-import com.shop.common.JsonData;
+import com.shop.utils.ThrowableUtil;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 /**
  * Description: 统一异常处理，包括【普通调用和ajax调用】
@@ -23,45 +19,41 @@ import javax.servlet.http.HttpServletResponse;
 @Slf4j
 public class GlobalExceptionHandler {
 
-	public static final String DEFAULT_ERROR_VIEW = "error";
-
-	@ExceptionHandler(value = Exception.class)
-	@ResponseBody
-	public JsonData errorHandler(HttpServletRequest request,
-								 HttpServletResponse response, Exception e) {
-		log.info(getClass().getName() + ".errorHandler】统一异常处理："+e.getMessage());
-		//1 获取错误状态码
-		HttpStatus httpStatus=getStatus(request);
-		log.info(getClass().getName() + ".errorHandler】统一异常处理!错误状态码httpStatus："+httpStatus);
-		//2 返回错误提示
-		log.error(e.getMessage(),e);
-//		e.printStackTrace();
-//		ExceptionEnum ee=getMessage(httpStatus);
-//		log.debug(getClass().getName() + ".errorHandler】统一异常处理：type="+ee.getType());
-//		log.debug(getClass().getName() + ".errorHandler】统一异常处理：code="+ee.getType());
-//		log.debug(getClass().getName() + ".errorHandler】统一异常处理：msg="+ee.getType());
-		if(StringUtils.isNotBlank(e.getMessage())){
-			return JsonData.fail(e.getMessage());
-		}
-		return JsonData.fail(e.toString());
+	/**
+	 * 处理所有不可知的异常
+	 */
+	@ExceptionHandler(Throwable.class)
+	public ResponseEntity<ApiError> handleException(Throwable e){
+		// 打印堆栈信息
+		log.error(ThrowableUtil.getStackTrace(e));
+		return buildResponseEntity(ApiError.error(e.getMessage()));
 	}
 
 	/**
-	 * 获取错误状态码
-	 * @param request
-	 * @return
+	 * BadCredentialsException
 	 */
-	private HttpStatus getStatus(HttpServletRequest request) {
-		Integer statusCode = (Integer) request
-				.getAttribute("javax.servlet.error.status_code");
-		if (statusCode == null) {
-			return HttpStatus.INTERNAL_SERVER_ERROR;
-		}
-		try {
-			return HttpStatus.valueOf(statusCode);
-		}
-		catch (Exception ex) {
-			return HttpStatus.INTERNAL_SERVER_ERROR;
-		}
+	@ExceptionHandler(BadCredentialsException.class)
+	public ResponseEntity<ApiError> badCredentialsException(BadCredentialsException e){
+		// 打印堆栈信息
+		String message = "坏的凭证".equals(e.getMessage()) ? "用户名或密码不正确" : e.getMessage();
+		log.error(message);
+		return buildResponseEntity(ApiError.error(message));
+	}
+
+	/**
+	 * 处理自定义异常
+	 */
+	@ExceptionHandler(value = BadRequestException.class)
+	public ResponseEntity<ApiError> badRequestException(BadRequestException e) {
+		// 打印堆栈信息
+		log.error(ThrowableUtil.getStackTrace(e));
+		return buildResponseEntity(ApiError.error(e.getStatus(),e.getMessage()));
+	}
+
+	/**
+	 * 统一返回
+	 */
+	private ResponseEntity<ApiError> buildResponseEntity(ApiError apiError) {
+		return new ResponseEntity<>(apiError, HttpStatus.valueOf(apiError.getStatus()));
 	}
 }
